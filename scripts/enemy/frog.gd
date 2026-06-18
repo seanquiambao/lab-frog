8ce5eb02cdad82004061cc65ed3f4e3abcac51de
seanquiambao <paparoxii03@gmail.com>
2026-06-17 22:22:29 -0700

    add aggro to enemy frog

--- scripts/enemy/frog.gd
+++ scripts/enemy/frog.gd
@@ -10,9 +10,9 @@ var is_aggressive: bool = false
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if(velocity.y == 0):
+	if velocity.y == 0:
 		velocity.x = lerp(velocity.x, 0.0, 5.0 * delta)
-	if is_on_floor() and timer.is_stopped():
+	if is_aggressive and is_on_floor() and timer.is_stopped():
 		timer.start()
 	move_and_slide()
 	_update_sprite()
