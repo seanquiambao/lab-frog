ed2144d6e88c9628aff2f70cde427a5cd7bcaec3
seanquiambao <paparoxii03@gmail.com>
2026-06-24 21:11:22 -0700

    add quit button

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -5,3 +5,6 @@ func _on_play_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/settings.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
